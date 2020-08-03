06672190565c3307a04ff160fb82412a472e90e5
Filipe <filipefernandes000@gmail.com>
2020-08-03 12:07:55 -0300

    Adicionar estrutura basica de uma arvore e inicializar a arvore binaria de busca

---
+++ Arvores/arvoreBinaria.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+
+typedef int bool;
+typedef int TIPOCHAVE;
+
+typedef struct aux {
+  TIPOCHAVE chave;
+  struct aux *esq, *dir;
+} NO;
+
+typedef NO* PONT;
---
+++ Arvores/arvoreBinariaBusca.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define true 1
+#define false 0
+
+typedef int bool;
+typedef int TIPOCHAVE;
+
+typedef struct aux {
+  TIPOCHAVE chave;
+  struct aux *esq, *dir;
+} NO;
+
+typedef NO* PONT;
+
+PONT inicializa() {
+  return NULL;
+};
+
+
+PONT adiciona(PONT raiz, PONT no) {
+  if (raiz == NULL) return no;
+  if(no->chave < raiz->chave) {
+    raiz->esq = adiciona(raiz->esq, no);
+  } else {
+    raiz->dir = adiciona(raiz->dir, no);
+  }
+  return raiz;
+}
+
+PONT criaNovoNo(TIPOCHAVE ch) {
+  PONT novoNo = (PONT) malloc(sizeof(NO));
+  novoNo->esq = NULL;
+  novoNo->dir = NULL;
+  novoNo->chave = ch;
+  return novoNo;
+}
+
+int main() {
+  PONT r = inicializa();
+  PONT no = criaNovoNo(23);
+  r = adiciona(r, no);
+  no = criaNovoNo(12);
+  r = adiciona(r, no);
+};
