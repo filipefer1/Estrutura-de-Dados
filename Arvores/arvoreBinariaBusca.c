96db2a508463de4666f29eb51be1c9358fd8add0
Filipe <filipefernandes000@gmail.com>
2020-08-04 11:34:08 -0300

    Implementar as funçoes de busca, contagem e exibicao de nos

--- Arvores/arvoreBinariaBusca.c
+++ Arvores/arvoreBinariaBusca.c
@@ -37,10 +37,53 @@ PONT criaNovoNo(TIPOCHAVE ch) {
   return novoNo;
 }
 
+PONT contem(TIPOCHAVE ch, PONT raiz) {
+  if (raiz == NULL) return NULL;
+  if (raiz->chave == ch) return raiz;
+  if (raiz->chave > ch) 
+    return contem(ch, raiz->esq);
+  return contem(ch, raiz->dir);
+}
+
+int numeroNos(PONT raiz) {
+  // busca do tipo esquerda-raiz-direita, também conhecida como varredura central
+  if (!raiz) return 0;
+  return numeroNos(raiz->esq) + 1 + numeroNos(raiz->dir);
+}
+
+void exibirArvore(PONT raiz) {
+  if (raiz != NULL) {
+    printf("%i", raiz->chave);
+    printf("(");
+    exibirArvore(raiz->esq);
+    exibirArvore(raiz->dir);
+    printf(")");
+  }
+} 
+
 int main() {
   PONT r = inicializa();
   PONT no = criaNovoNo(23);
   r = adiciona(r, no);
   no = criaNovoNo(12);
   r = adiciona(r, no);
+  no = criaNovoNo(8);
+  r = adiciona(r, no);
+  no = criaNovoNo(14);
+  r = adiciona(r, no);
+  no = criaNovoNo(25);
+  r = adiciona(r, no);
+  no = criaNovoNo(32);
+  r = adiciona(r, no);
+  no = criaNovoNo(41);
+  r = adiciona(r, no);
+  no = criaNovoNo(5);
+  r = adiciona(r, no);
+  no = criaNovoNo(21);
+  r = adiciona(r, no);
+
+  PONT c = contem(5, r);
+  printf("%i\n", c->chave);
+  printf("%d\n", numeroNos(r));
+  exibirArvore(r);
 };
